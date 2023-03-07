b9135544c7ce75f0ee6d232847b6c169798afcbf
Agus Deo Saputra Utama <81843382+agusdeo@users.noreply.github.com>
2023-03-07 18:22:25 +0700

    update login.js

--- e2e/login.js
+++ e2e/login.js
@@ -1,16 +1,13 @@
+import loginPage from './loginPage'
 const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor')
 
 Given('I open login page', () => {
-	cy.visit('http://zero.webappsecurity.com/login.html')
+	loginPage.visit()
 })
 When('I submit login', () => {
-	cy.get('#user_login').clear()
-	cy.get('#user_login').type('username')
-
-	cy.get('#user_password').clear()
-	cy.get('#user_password').type('password')
-
-	cy.get('input[name="submit"]').click()
+	loginPage.fillUsername('username')
+	loginPage.fillPassword('password')
+	loginPage.sign_in()
 })
 
 Then('I should see homepage', () => {
